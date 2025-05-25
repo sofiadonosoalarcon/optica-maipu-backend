bb1e3c87d49f4737b8281e2c3b46b4dd58b88c7c
Sofia Donoso MTT <sdonoso@mtt.gob.cl>
2025-05-25 16:59:54 -0400

    Se mejora la string de conexion de la base de datos

--- app/infraestructure/utils/db.py
+++ app/infraestructure/utils/db.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mssql+pyodbc://adminbd:Admin2025@proyectobd.database.windows.net/optica-maipu?driver=ODBC+Driver+17+for+SQL+Server"
+# DATABASE_URL = "mssql+pyodbc://adminbd:Admin2025@proyectobd.database.windows.net/optica-maipu?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URL = (
+    "mssql+pyodbc://adminbd:Admin2025@proyectobd.database.windows.net:1433"
+    "/?database=optica-maipu&driver=ODBC+Driver+17+for+SQL+Server"
+)
 
 engine = create_engine(DATABASE_URL, echo=True, fast_executemany=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
