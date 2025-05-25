e12f20e9a2f52b69576c6a24d733b8a8db4ac7bb
Sofia Constanza Donoso Alarcón <sdonoso@mtt.gob.cl>
2025-05-25 16:34:40 -0400

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,4 @@
-from flask import Flask
+"""from flask import Flask
 from adapters.input.flask_app.routes import register_routes
 
 def create_app():
@@ -8,4 +8,17 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
+    app.run(debug=True)"""
+
+from flask import Flask
+from adapters.input.flask_app.routes import register_routes
+
+def create_app():
+    app = Flask(__name__)
+    register_routes(app)
+    return app
+
+app = create_app()  # 👈 Esto es necesario para gunicorn
+
+if __name__ == '__main__':
     app.run(debug=True)
